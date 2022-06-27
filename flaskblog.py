997cae5a40509e34dec670955e5ecf7c6c84357c
Derek Chen <derekzc99@gmail.com>
2022-06-26 23:20:54 -0700

    commit three: form flash

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -31,14 +31,25 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
-@app.route("/register", methods=['GET', 'POST']) # list specifying methods allowed
+@app.route("/register", methods=['GET', 'POST']) # list specifying allowed methods
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        # bootstrap has different alert styles for success, warning, and danger
+        return redirect(url_for('home'))
+        # again, home refers to the FUNCTION name in line 25 and not the url path
     return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check username and password.', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 # this conditional is only true if we run the script with python directly;
