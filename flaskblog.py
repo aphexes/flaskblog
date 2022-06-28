0e56bf459d83820ea7a8b557fffd7f4b02984b1b
Derek Chen <derekzc99@gmail.com>
2022-06-27 20:24:44 -0700

    04 SQLAlchemy database

--- flaskblog.py
+++ flaskblog.py
@@ -1,9 +1,41 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'e8883e90b0d88a02978811153ba747a6'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# 3 /s specify a relative path; site.db file should get created in our project directory
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id         = db.Column(db.Integer, primary_key=True)    # primary_key is a unique id for our user
+    username   = db.Column(db.String(20), unique=True, nullable=False)   # 20 character max length, must be unique, can't null bc username required
+    email      = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password   = db.Column(db.String(60), nullable=False)
+    posts      = db.relationship('Post', backref='author', lazy=True)    # runs in the background b/c not a column
+    # backref allows us to use the author attribute to get the user who created the post
+
+    def __repr__(self):     # __repr__ is a special method used to represent a class’s objects as a string
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+    id          = db.Column(db.Integer, primary_key=True)
+    title       = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    # current time req datetime module; no parentheses at end of datetime.utcnow in order to pass the function as an argument and not the current time
+    # if we run it with parentheses, the time you set it will permanently be the time displayed
+    content     = db.Column(db.Text, nullable=False)
+    user_id     = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) # user.id is going to be the user who authored the post
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+# post model and user model will have a relationship since users will author posts; this is a one-to-many relationship
+# bc a users can have many posts but a post can only have one user; in the User class we are s
+
 
 posts = [
     {
