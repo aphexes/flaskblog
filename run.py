c6eab4e58ee4d070cdca290545be0f1d633b5cd4
Derek Chen <derekzc99@gmail.com>
2022-07-17 18:51:45 -0700

    12 custom error pages

--- run.py
+++ run.py
@@ -6,3 +6,4 @@ if __name__ == '__main__':
     # this conditional is only true if we run the script with python directly;
     # if we import the module to somewhere else, the name will be the name of the module
     app.run(debug=True)
+
